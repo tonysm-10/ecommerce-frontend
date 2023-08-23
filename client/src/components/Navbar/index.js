4c98d30c9f3798638cbb42418566bf15512c21b8
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-08-22 17:59:20 -0700

    fixed error

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import './index.css';
 const Navbar = function ({removeFromCart, cart, openSidebar, closeSidebar, isOpen }) {
   const sidebar = (
     <div className='sidebar slide-from-right'>
-      <a href='#' onClick={closeSidebar}>x</a>
+      <a href='/#' onClick={closeSidebar}>x</a>
       <h3>CART</h3>
       {cart.map((product) => (
         <div className='cart' key={product.id}>
@@ -25,10 +25,10 @@ const Navbar = function ({removeFromCart, cart, openSidebar, closeSidebar, isOpe
       <nav className='navbar'>
         <h1 className='title'>GYM ESSENTIALS</h1>
         <div className='nav-links'>
-          <a onClick={openSidebar} className='link'>
+          <a href='/#' onClick={openSidebar} className='link'>
             cart
           </a>
-          <a className='link'>contact</a>
+          <a href='/#' className='link'>contact</a>
         </div>
       </nav>
     </div>
